e646fdc2fc992379f86ad6ed2e6b138a5e0e61aa
Garrett Holmes <Garrett.Holmes@gmail.com>
2017-02-13 22:17:08 -0600

    Starting server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
+import http from 'http';
 
 let app = express();
 app.use(express.static('client'));
+app.listen(3000, () => {
+	console.log('Listening on port 3000...');
+});
