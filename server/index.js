f85b41a0e8fe9b3e247199640e7d756111a0e2f9
Jimmy Stevenson <jimmyastevenson@gmail.com>
2017-02-13 21:56:14 -0600

    basic package updates for react

--- server/index.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-var server = http.createServer(function(req, res){
-  res.end('app is alive');
-})
-
-server.listen(port='3006', function(){
-  console.log('listening on port 3006');
-})
